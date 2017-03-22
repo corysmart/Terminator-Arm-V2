8e88d5e0f2f5b10403872c42dbb756e4895191b8
Cory Bethrant <corysmart@comcast.net>
2017-03-22 17:56:12 -0400

    Update Classification.cpp

--- Classification.cpp
+++ Classification.cpp
@@ -6,14 +6,87 @@
 //
 //
 
-#include <stdio.h>
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <math.h>
-using namespace std;
+//
+//  main.cpp
+//  Terminator Arm V2
+//
+//  Copyright © 2017 Team Terminator. All rights reserved.
+//
 
-int main()
+#include <array>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include "myo.hpp"
+class DataCollector : public myo::DeviceListener {
+public:
+    DataCollector()
+    : emgSamples()
+    {
+    }
+    // onUnpair() is called whenever the Myo is disconnected from Myo Connect by the user.
+    void onUnpair(myo::Myo* myo, uint64_t timestamp)
+    {
+        // We've lost a Myo.
+        // Let's clean up some leftover state.
+        emgSamples.fill(0);
+    }
+    // onEmgData() is called whenever a paired Myo has provided new EMG data, and EMG streaming is enabled.
+    void onEmgData(myo::Myo* myo, uint64_t timestamp, const int8_t* emg)
+    {
+        for (int i = 0; i < 8; i++) {
+            emgSamples[i] = emg[i];
+        }
+    }
+    // There are other virtual functions in DeviceListener that we could override here, like onAccelerometerData().
+    // For this example, the functions overridden above are sufficient.
+    // We define this function to print the current values that were updated by the on...() functions above.
+    void print()
+    {
+        // Clear the current line
+        std::cout << '\r';
+        // Print out the EMG data.
+        for (size_t i = 0; i < emgSamples.size(); i++) {
+            std::ostringstream oss;
+            oss << static_cast<int>(emgSamples[i]);
+            std::string emgString = oss.str();
+            std::cout << '[' << emgString << std::string(4 - emgString.size(), ' ') << ']';
+        }
+        std::cout << std::flush;
+    }
+    // The values of this array is set by onEmgData() above.
+    std::array<int8_t, 8> emgSamples;
+};
+int main(int argc, char** argv)
+{
+    // We catch any exceptions that might occur below -- see the catch statement for more details.
+    try {
+        // First, we create a Hub with our application identifier. Be sure not to use the com.example namespace when
+        // publishing your application. The Hub provides access to one or more Myos.
+        myo::Hub hub("com.example.emg-data-sample");
+        std::cout << "Attempting to find a Myo..." << std::endl;
+        // Next, we attempt to find a Myo to use. If a Myo is already paired in Myo Connect, this will return that Myo
+        // immediately.
+        // waitForMyo() takes a timeout value in milliseconds. In this case we will try to find a Myo for 10 seconds, and
+        // if that fails, the function will return a null pointer.
+        myo::Myo* myo = hub.waitForMyo(10000);
+        // If waitForMyo() returned a null pointer, we failed to find a Myo, so exit with an error message.
+        if (!myo) {
+            throw std::runtime_error("Unable to find a Myo!");
+        }
+        // We've found a Myo.
+        std::cout << "Connected to a Myo armband!" << std::endl << std::endl;
+        // Next we enable EMG streaming on the found Myo.
+        myo->setStreamEmg(myo::Myo::streamEmgEnabled);
+        // Next we construct an instance of our DeviceListener, so that we can register it with the Hub.
+        DataCollector collector;
+        // Hub::addListener() takes the address of any object whose class inherits from DeviceListener, and will cause
+        // Hub::run() to send events to all registered device listeners.
+        hub.addListener(&collector);
+        // Finally we enter our main loop.
+        while (1) {
+            int main()
 {
     // Function receiving an array of data with 240 integers.
     // i.e an 80-emg x 30-Datalines array every sec. or so at run-time
@@ -71,3 +144,20 @@ int main()
     for (int i = 0; i < 6; i++){
         if (Percents[4][i + 1] < 90.0) Flag = 1;
     } if (!Flag) Rest = 4.0;
+
+            hub.run(1000/20);
+            // After processing events, we call the print() member function we defined above to print out the values we've
+            // obtained from any events that have occurred.
+            collector.print();
+                cout << "Current Gesture: ";
+        }
+        // If a standard exception occurred, we print out its message and exit.
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        std::cerr << "Press enter to continue.";
+        std::cin.ignore();
+        return 1;
+    }
+}
+
+
